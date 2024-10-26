b3b3b500e55cac24edb8965bd858cf3b1eb14a96
Azadeh Divsalar <azadeh.divsalar@gmail.com>
2024-10-26 11:15:26 +0330

    fix Login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -3,7 +3,7 @@
     <div class="q-gutter-md q-pa-md flex flex-center">
       <q-card style="max-width: 400px; width: 100%">
         <q-card-section>
-          <q-form  @submit.prevent="handleLogin" ref="loginForm">
+          <q-form  @submit.prevent="onSubmit" ref="loginForm">
             <q-input filled v-model="username" label="Username" />
             <q-input filled v-model="password" label="Password" type="password"  outlined/>
             <div class="q-mt-md text-center">
@@ -18,35 +18,35 @@
 </template>
 <script setup>
 import { ref } from 'vue'
-import * as Yup from 'yup'
+// import * as Yup from 'yup'
 import { useAuthStore } from 'src/stores/auth'
-// import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
 
 const username = ref('')
 const password = ref('')
 const authStore = useAuthStore()
-// const router = useRouter()
+const router = useRouter()
 const errorMessage = ref('')
 
-const loginSchema = Yup.object({
-  username: Yup.string().required('Username is required'),
-  password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required')
-})
-const handleLogin = async () => {
-  try {
-    await loginSchema.validate({ username: username.value, password: password.value })
-    await authStore.login({ username: username.value, password: password.value })
-  } catch (error) {
-    errorMessage.value = error.message
-  }
-}
-
-// const onSubmit = () => {
-//   authStore.login(username.value, password.value)
-//   if (authStore.isAuthenticated) {
-//     router.push('/dashboard')
+// const loginSchema = Yup.object({
+//   username: Yup.string().required('Username is required'),
+//   password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required')
+// })
+// const handleLogin = async () => {
+//   try {
+//     await loginSchema.validate({ username: username.value, password: password.value })
+//     await authStore.login({ username: username.value, password: password.value })
+//   } catch (error) {
+//     errorMessage.value = error.message
 //   }
 // }
+
+const onSubmit = () => {
+  authStore.login(username.value, password.value)
+  if (authStore.isAuthenticated) {
+    router.push('/dashboard')
+  }
+}
 </script>
 
 <style scoped>
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -14,9 +14,11 @@
 // })
 
 import { defineStore } from 'pinia'
+// import axios from axios
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
+    // tokenquasar :null,
     isAuthenticated: localStorage.getItem('isAuthenticated') === 'true'
   }),
   actions: {
